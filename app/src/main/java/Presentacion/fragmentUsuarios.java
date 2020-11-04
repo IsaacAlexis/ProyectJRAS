35015527676faa383b40d2ab23c5ec1b628ae1a1
BrayanEspinozaF <BrayanEspinozaF@users.noreply.github.com>
2020-11-03 19:38:15 -0700

    Interfaz Modificar Usuarios

--- app/src/main/java/Presentacion/fragmentUsuarios.java
+++ app/src/main/java/Presentacion/fragmentUsuarios.java
@@ -7,9 +7,14 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.example.jras.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,6 +25,8 @@ import static androidx.navigation.Navigation.findNavController;
 public class fragmentUsuarios extends Fragment {
 
     public FloatingActionButton fabUsuario;
+    public ImageView btnModificar, btnBuscar;
+    public EditText txtNombre;
 
     public fragmentUsuarios() {
         // Required empty public constructor
@@ -30,9 +37,38 @@ public class fragmentUsuarios extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view= inflater.inflate(R.layout.fragment_usuarios, container, false);
+        final View view = inflater.inflate(R.layout.fragment_usuarios, container, false);
 
-        fabUsuario= view.findViewById(R.id.fabAgregarUsuario);
+        fabUsuario = view.findViewById(R.id.fabAgregarUsuario);
+
+        btnModificar=view.findViewById(R.id.btnModificarUsuario);
+
+        txtNombre=view.findViewById(R.id.txtBuscarUsuario);
+
+        txtNombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                btnModificar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnModificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                findNavController(v).navigate(R.id.fragementModificarUsuarios);
+            }
+        });
 
         fabUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,9 +79,13 @@ public class fragmentUsuarios extends Fragment {
 
             }
         });
-
         return view;
+
+
+
+
     }
+
 
 
 }

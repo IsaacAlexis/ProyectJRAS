c72da76910a6628a66cc28c93a22df8f9e51f40a
EbberDominguez <l16540480@delicias.tecnm.mx>
2020-11-13 20:58:49 -0700

    Prueba de modifiacar usuarios
    Se realiza la prueba de modificar usuarios.

---
+++ app/src/main/java/Data/Models/HouseModel.java
@@ -0,0 +1,110 @@
+package Data.Models;
+
+import android.provider.ContactsContract;
+
+import java.math.BigInteger;
+
+public class HouseModel {
+    public String barCode;
+    public String owner="Adrian Dominguez";
+    public BigInteger phoneNumber=new BigInteger("6391722102");
+    public String email="ebberdominguez@gmail.com";
+    public String street="calle 24 de febrero";
+    public Integer houseNumber=Integer.parseInt("203");
+    public Integer zipCode=Integer.parseInt("33029");
+    public String  colony="Col. Obrera";
+    public String city="Delicias";
+    public String state="Chihuahua";
+    public String statusHouse="Al corriente";
+
+    public HouseModel() {
+    }
+
+    public String getBarCode() {
+        return barCode;
+    }
+
+    public void setBarCode(String barCode) {
+        this.barCode = barCode;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public BigInteger getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(BigInteger phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public Integer getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(Integer houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public Integer getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(Integer zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getColony() {
+        return colony;
+    }
+
+    public void setColony(String colony) {
+        this.colony = colony;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStatusHouse() {
+        return statusHouse;
+    }
+
+    public void setStatusHouse(String statusHouse) {
+        this.statusHouse = statusHouse;
+    }
+}
--- app/src/main/java/com/example/jras/fragmentModificarViviendas.java
+++ app/src/main/java/com/example/jras/fragmentModificarViviendas.java
@@ -12,11 +12,50 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import Data.Models.HouseModel;
 
 public class fragmentModificarViviendas extends Fragment {
 
     private FragmentModificarViviendasViewModel mViewModel;
+    private EditText owner;
+    private EditText phoneNumber;
+    private EditText email;
+    private EditText street;
+    private EditText houseNumber;
+    private EditText zipCode;
+    private EditText colony;
+    private EditText city;
+    private EditText state;
+    private TextView statusHouse;
+    HouseModel house=new HouseModel();
+    public void assignValues(){
+        owner.setText(house.getOwner());
+        phoneNumber.setText(house.getPhoneNumber().toString());
+        email.setText(house.getEmail());
+        street.setText(house.getStreet());
+        houseNumber.setText(house.getHouseNumber().toString());
+        zipCode.setText(house.getZipCode().toString());
+        colony.setText(house.getColony());
+        city.setText(house.getCity());
+        state.setText(house.getState());
+        statusHouse.setText(house.getStatusHouse());
+    }
 
+    public void getvalues(View view){
+        owner= view.findViewById(R.id.txtPropietario);
+        phoneNumber=view.findViewById(R.id.txtTelefonoViv);
+        email=view.findViewById(R.id.txtEmail);
+        street=view.findViewById(R.id.txtCalleViv);
+        houseNumber= view.findViewById(R.id.txtNumViv);
+        zipCode= view.findViewById(R.id.txtCPViv);
+        colony= view.findViewById(R.id.txtColViv);
+        city= view.findViewById(R.id.txtCiudadViv);
+        state= view.findViewById(R.id.txtEstadoViv);
+        statusHouse= view.findViewById(R.id.lbStatus);
+        assignValues();
+    }
     public static fragmentModificarViviendas newInstance() {
         return new fragmentModificarViviendas();
     }
@@ -24,10 +63,9 @@ public class fragmentModificarViviendas extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-         final View view= inflater.inflate(R.layout.fragment_modificar_viviendas, container, false);
-        EditText example=view.findViewById(R.id.txtPropietario);
-        example.setText("Hola");
-         return view;
+        final View view= inflater.inflate(R.layout.fragment_modificar_viviendas, container, false);
+        getvalues(view);//Se obtienen los datos a modificar
+        return view;
     }
 
     @Override

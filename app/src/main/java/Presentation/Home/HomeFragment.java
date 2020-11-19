19853e03a13754a88a015dcf527865e3a4f73ff6
Brayan <bryan.espinoza97@hotmail.com>
2020-11-19 16:35:46 -0700

    interfaz de consumo

--- app/src/main/java/Presentation/Home/HomeFragment.java
+++ app/src/main/java/Presentation/Home/HomeFragment.java
@@ -10,8 +10,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.jras.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import static androidx.navigation.Navigation.findNavController;
 
 public class HomeFragment extends Fragment {
+    public FloatingActionButton fabConsumo;
 
     private HomeViewModel homeViewModel;
 
@@ -19,8 +23,17 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View view = inflater.inflate(R.layout.fragment_home, container, false);
 
-        return root;
+        fabConsumo=view.findViewById(R.id.fabConsumo);
+        fabConsumo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findNavController(view).navigate(R.id.fagmentConsumo);
+
+            }
+        });
+
+        return view;
     }
 }

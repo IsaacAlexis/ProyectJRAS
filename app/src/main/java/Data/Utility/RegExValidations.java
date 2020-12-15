f866c6e1b22c84e22feef677be9041e0100d5804
IsaacAlexis <alexxis.mal@gmail.com>
2020-12-14 17:08:06 -0700

    Cambios nuevo s- Cosas Pendientes

---
+++ app/src/main/java/Data/Utility/RegExValidations.java
@@ -0,0 +1,17 @@
+package Data.Utility;
+
+public class RegExValidations {
+
+    public String validEmail = "^[A-Z_a-z0-9-]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9-]+)*(.[A-Za-z]{2,4})$";
+    public String validNamesComplete = "^[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,20}(\\s[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,20})(\\s[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,20})?(\\s[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,20})?$";
+    public String validCityState = "^[a-zA-ZÁÉÍÓÚñáéíóúÑ.]{1,10}(\\s[a-zA-ZÁÉÍÓÚñáéíóúÑ.]{1,10})?(\\s[a-zA-ZÁÉÍÓÚñáéíóúÑ.]{1,10})?(\\s[a-zA-ZÁÉÍÓÚñáéíóúÑ.]{1,10})?$";
+    public String validNameLstName = "^[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,25}(\\s[A-Za-zÁÉÍÓÚñáéíóúÑ]{1,25})?$";
+    public String validUser = "^(?=.*[0-9])[0-9a-zA-Z]{8,15}$";
+    public String validPassword = "^(?=.*\\d)(?=.*[\\u0021-\\u002b\\u003c-\\u0040])(?=.*[A-Z])(?=.*[a-z])\\S{8,16}$";
+
+
+    public String ValidNumbers(int maxNum,int minNum){
+        String validNumbers = "^\\d{"+minNum+","+maxNum+"}$";
+        return validNumbers;
+    }
+}

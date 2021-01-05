47c175889524826270480dd4d306dd17265f01d1
Kevin Antonio Bojorquez Alvarado <l16540256@delicias.tecnm.mx>
2021-01-04 18:23:22 -0700

    Interfaz reportes mocha

--- app/src/main/java/Presentation/Reports/fragmentReportes.java
+++ app/src/main/java/Presentation/Reports/fragmentReportes.java
@@ -1,5 +1,8 @@
 package Presentation.Reports;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +10,137 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.jras.R;
+import java.util.Calendar;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragmentReportes#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class fragmentReportes extends Fragment {
+    boolean bandera;
+    //Variables
+    EditText Fecha;
+    EditText SegundaFecha;
+    TextView MostrarFecha;
+    TextView MostrarSegundaFecha;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    DatePickerDialog.OnDateSetListener setListener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public fragmentReportes() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragmentReportes.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragmentReportes newInstance(String param1, String param2) {
-        fragmentReportes fragment = new fragmentReportes();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public void getValues(View view) {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        //Relacionar variables con los componentes
+        Fecha = view.findViewById(R.id.txtFecha);
+        MostrarFecha = view.findViewById(R.id.txtCapturaFecha);
+        SegundaFecha = view.findViewById(R.id.txtFecha2);
+        MostrarSegundaFecha = view.findViewById(R.id.txtCapturaFecha2);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reportes, container, false);
+        View view = inflater.inflate(R.layout.fragment_reportes, container, false);
+        getValues(view);
+        //crear instancia del calendario
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+        Calendar calendar2 = Calendar.getInstance();
+        final int year2 = calendar2.get(Calendar.YEAR);
+        final int month2 = calendar2.get(Calendar.MONTH);
+        final int day2 = calendar2.get(Calendar.DAY_OF_MONTH);
+
+        Fecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext() ,android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+                bandera = false;
+
+            }
+        });
+
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                if (!bandera){
+                    month = month+1;
+                    String date = day+"/"+month+"/"+year;
+                    Fecha.setText(date);
+                }
+                else {
+                    month = month+1;
+                    String date = day+"/"+month+"/"+year;
+                    SegundaFecha.setText(date);
+                }
+            }
+        };
+
+       /* MostrarFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        MostrarFecha.setText(date);
+                    }
+                },year,month,day);
+
+            }
+        });*/
+
+        //Segundo calendario
+       SegundaFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext() ,android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        ,setListener,year,month,day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                datePickerDialog.show();
+                bandera = true;
+
+            }
+        });
+
+
+
+        /*MostrarSegundaFecha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        //month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        MostrarSegundaFecha.setText(date);
+                    }
+                },year,month,day);
+
+            }
+        });*/
+        return view;
+
+
+
     }
+
+
 }
